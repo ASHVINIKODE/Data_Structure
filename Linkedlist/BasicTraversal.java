c8facb0916997ba7cb3de9e3200a8b6166fa28b3
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2026-04-26 13:35:16 +0530

    initial commit

---
+++ Linkedlist/BasicTraversal.java
@@ -0,0 +1,38 @@
+
+class BasicTraversal {
+
+   static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+
+        Node head;
+
+        void traverse() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        // BasicTraversal obj = new BasicTraversal(); static
+        LinkedList list = new LinkedList();
+
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.traverse();
+    }
+}
---
+++ Linkedlist/LinkedListDemo.java
@@ -0,0 +1,11 @@
+class LinkedListDemo {
+ static class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+ }
+ 
+}
