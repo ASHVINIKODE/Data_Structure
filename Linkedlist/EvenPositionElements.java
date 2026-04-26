29e62c744e5a4da74d98997d9a993bb3ad188359
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2026-04-27 01:05:25 +0530

    initial commit

--- Linkedlist/EvenPositionElements.java
+++ Linkedlist/EvenPositionElements.java
@@ -1,3 +1,56 @@
+
 public class EvenPositionElements {
-    
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+
+        Node head;
+
+        void printEvenPos(Node head) {
+            Node temp = head;
+            int i = 1;
+            while (temp != null) {
+                if (i % 2 == 0) {
+                    System.out.print(temp.data+" ");
+
+                }
+                temp = temp.next;
+                i++;
+            }
+        }
+
+        void traverse() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.print("Null");
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(10);
+        list.head.next = new Node(20);
+        list.head.next.next = new Node(30);
+        list.head.next.next.next = new Node(40);
+        list.head.next.next.next.next = new Node(50);
+        list.traverse();
+        System.out.println();
+        System.out.println("Even Position element:");
+        list.printEvenPos(list.head);
+
+    }
+
 }
