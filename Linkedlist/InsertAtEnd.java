165cd8edb5c9a18a9f47c10c1f6c2213b2666b5f
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2026-04-27 13:04:25 +0530

    initial commit

---
+++ Linkedlist/InsertAtEnd.java
@@ -0,0 +1,53 @@
+
+public class InsertAtEnd {
+
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+
+        Node head;
+
+        void insertAtEnd(int value) {
+            Node newNode = new Node(value);
+            if (head == null) {
+                head = newNode;
+                return;
+            }
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+
+        void traverse() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+
+        list.traverse();
+    }
+}
